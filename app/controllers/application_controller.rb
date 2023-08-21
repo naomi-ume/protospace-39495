a9e78dc75b844d703106683f1129a6346668fe33
naomi-ume <naomi.sue.tate@gmail.com>
2023-08-21 15:39:27 +0900

    消し忘れ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_url_options
+
   
   private
   def configure_permitted_parameters
